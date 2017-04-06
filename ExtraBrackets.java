e9c8e904bdbddcf3e3f00fd675e994791a705c51
reddy1289 <inandipa@uncc.edu>
2017-04-05 20:42:37 -0400

    Stack has been added to get better results.

--- ExtraBrackets.java
+++ ExtraBrackets.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,41 +13,48 @@ public class ExtraBrackets {
 		BufferedReader br = new BufferedReader(file);
 		Pattern indentation_pattern = Pattern.compile("(\\s*)(.*)");
 		Pattern bracket_end_pattern = Pattern.compile("(\\s*)\\}");
-		Pattern bracket_start_pattern = Pattern.compile("(\\s*)(.+)\\{");
+		Pattern bracket_start_pattern = Pattern.compile("(\\s*)(.*)\\{");
+		Pattern empty_line_pattern = Pattern.compile("^(\\s*)$");
+		Stack<String> stack = new Stack<String>();
 		String line = "";
 		String Pre_line = "";
 		int Pre_line_indentation_lenght = 0, line_indentation_length=0;
 		int line_number = 0;
 		Matcher m;
-		int bracket_count = 0;
 		while((line = br.readLine()) != null){
 			line_number++;
 			line = FIleCheckForUnrequiredPatterns.LineCheck(line);
-			if(line.equals("") ){
+			m = empty_line_pattern.matcher(line);
+			 if (m.find( )) {
 				continue;
-			}
+			 } 
 			
 			m = bracket_start_pattern.matcher(line);
 			 while (m.find( )) {
-				 //System.out.println(line + " "+"in");
-				bracket_count++;
+				stack.push("{");
 			 } 
 
 			 m = bracket_end_pattern.matcher(line);
 			while(m.find( )) {
-				 //System.out.println(line + " dec");
-				bracket_count--;
+				if(stack.empty()){
+					System.out.println("Sorce file have missing {");
+				}else{
+					stack.pop();
+				}
 			 }
 			
-			
+			//System.out.println(line);
 			m= indentation_pattern.matcher(line);
 			if(m.find( )) {
+				
 				 String s = m.group(1);
 				 line_indentation_length = s.length();
 			 }
+			
 			if(Pre_line_indentation_lenght < line_indentation_length && line_number != 1){
 				m= bracket_start_pattern.matcher(Pre_line);
 				if(!m.find( )) {
+
 					System.out.println("open bracket missing at line:"+ --line_number);
 					line_number++;
 				 }
@@ -55,6 +63,7 @@ public class ExtraBrackets {
 			if(Pre_line_indentation_lenght > line_indentation_length  && line_number != 1){
 				m= bracket_end_pattern.matcher(line);
 				if(!m.find( )) {
+					
 					System.out.println("close bracket missing at line:"+ line_number);
 				 }
 			}
@@ -63,14 +72,10 @@ public class ExtraBrackets {
 			Pre_line_indentation_lenght = line_indentation_length;
 		}
 		br.close();
-		if(bracket_count < 0){
-			System.out.println("the file has "+ Math.abs(bracket_count)+ " extra close bracket");
-			
-		}else if(bracket_count > 0){
-			System.out.println("the file has "+ Math.abs(bracket_count)+ " extra open bracket");
-		}else{
-			System.out.println("the file dont have any extra brackets");
+		if(!stack.empty()){
+			System.out.println("Sorce file have missing } ");
 		}
+		
 		
 	}
 
